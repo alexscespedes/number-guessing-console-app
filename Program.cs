2bd0fce722aa0af8f9134e885df97ed795bf0d8b
alexscespedes <alexandersenc99@gmail.com>
2025-04-18 07:00:46 -0400

    Add while structure with feedback if the input is too high or too low

--- Program.cs
+++ Program.cs
@@ -5,9 +5,31 @@
         static void Main(string[] args)
         {
             Random random = new Random();
-            Console.WriteLine("Welcome to the Number Guessing Game!");
             int randomNumber = random.Next(1,100);
-            Console.WriteLine("Guess the number: " + randomNumber);
+            bool exit = false;
+            while (!exit) 
+            {
+                Console.WriteLine("Welcome to the Number Guessing Game!");
+                Console.Write("Try to guess the number: ");
+                if (!int.TryParse(Console.ReadLine(), out int userGuess)) 
+                {
+                    Console.WriteLine("Invalid input! Please enter a valid integer");
+                }
+
+                if (userGuess > randomNumber)
+                {
+                    Console.WriteLine("The number is lower.");
+                }
+                else if (userGuess < randomNumber) 
+                {
+                    Console.WriteLine("The number is higher.");
+                }
+                else {
+                    Console.WriteLine("Your guessed it. Congratulations!");
+                    exit = true;
+                }
+            }
+
 
         }
     }
