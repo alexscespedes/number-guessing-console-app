7b33ae2b2668c25211891e4fd068910920e6875b
alexscespedes <alexandersenc99@gmail.com>
2025-04-18 12:19:44 -0400

    Update the structure logic with the game validations

--- Program.cs
+++ Program.cs
@@ -5,25 +5,20 @@
         static void Main(string[] args)
         {
             Random random = new Random();
-            int randomNumber = random.Next(1,100);
+            int randomNumber = random.Next(1,10);
             int userGuess;
-            int score = 10;
+            int limit = 10;
             int numberOfAttempts = 0;
             bool exit = false;
-
-
-            // Try out a list of <int>
 
             // Implement different difficulty levels (different ranges): 
             // Easy: 1,100  
             // Medium: 1, 1000,
             // Hard: 1, 10000
-            // Keep track of high scores (fewest guesses)
 
-
+            Console.WriteLine("Welcome to the Number Guessing Game!");
             while (!exit) 
             {
-                Console.WriteLine("Welcome to the Number Guessing Game!");
                 Console.Write("Try to guess the number: ");
                 while (true)
                 {
@@ -35,26 +30,30 @@
                     Console.WriteLine("Invalid input! Please enter a valid number."); 
                 }
 
-
-                if (userGuess > randomNumber)
+                if (userGuess == randomNumber)
                 {
-                    Console.WriteLine("Too low!");
-                    score--;
-                    numberOfAttempts++;
-                    Console.WriteLine(numberOfAttempts);
-                }
-                if (userGuess < randomNumber) 
-                {
-                    Console.WriteLine("Too high!");
-                    score--;
-                    numberOfAttempts++;
-                    Console.WriteLine(numberOfAttempts);
-                }
-                else {
                     Console.WriteLine("Your guessed it. Congratulations!");
-                    int highscore = score;
-                    Console.WriteLine($"Your High Score was: {highscore}");
+                    int highscore = limit - numberOfAttempts;
+                    Console.WriteLine($"Your highscore was: {highscore}");
                     exit = true;
+                }
+
+                else if (userGuess != randomNumber)
+                {
+                    numberOfAttempts++;
+                    if (numberOfAttempts < limit)
+                    {
+                        string message = (userGuess > randomNumber) ? "Too high" : "Too low";
+                        Console.WriteLine(message);
+                        // score--;
+                        Console.WriteLine($"Number of guesses made: {numberOfAttempts}");
+                    }
+                    else 
+                    {
+                        Console.WriteLine("You runs out of attempts");
+                        exit = true;
+                    }
+                    
                 }
             }
 
