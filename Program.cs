05cda0d622368d43bc4e827703ca215770353c7c
alexscespedes <alexandersenc99@gmail.com>
2025-04-18 11:31:09 -0400

    Fix user prompt to guess the number

--- Program.cs
+++ Program.cs
@@ -6,26 +6,54 @@
         {
             Random random = new Random();
             int randomNumber = random.Next(1,100);
+            int userGuess;
+            int score = 10;
+            int numberOfAttempts = 0;
             bool exit = false;
+
+
+            // Try out a list of <int>
+
+            // Implement different difficulty levels (different ranges): 
+            // Easy: 1,100  
+            // Medium: 1, 1000,
+            // Hard: 1, 10000
+            // Keep track of high scores (fewest guesses)
+
+
             while (!exit) 
             {
                 Console.WriteLine("Welcome to the Number Guessing Game!");
                 Console.Write("Try to guess the number: ");
-                if (!int.TryParse(Console.ReadLine(), out int userGuess)) 
+                while (true)
                 {
-                    Console.WriteLine("Invalid input! Please enter a valid integer");
+                    Console.Write("Try to guess the number: ");
+                    if (int.TryParse(Console.ReadLine(), out userGuess))
+                    {
+                        break;
+                    }
+                    Console.WriteLine("Invalid input! Please enter a valid number."); 
                 }
+
 
                 if (userGuess > randomNumber)
                 {
-                    Console.WriteLine("The number is lower.");
+                    Console.WriteLine("Too low!");
+                    score--;
+                    numberOfAttempts++;
+                    Console.WriteLine(numberOfAttempts);
                 }
-                else if (userGuess < randomNumber) 
+                if (userGuess < randomNumber) 
                 {
-                    Console.WriteLine("The number is higher.");
+                    Console.WriteLine("Too high!");
+                    score--;
+                    numberOfAttempts++;
+                    Console.WriteLine(numberOfAttempts);
                 }
                 else {
                     Console.WriteLine("Your guessed it. Congratulations!");
+                    int highscore = score;
+                    Console.WriteLine($"Your High Score was: {highscore}");
                     exit = true;
                 }
             }
