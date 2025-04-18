b63ac7587bf5a6ece68d17b3baea16e295103dd6
alexscespedes <alexandersenc99@gmail.com>
2025-04-18 12:56:45 -0400

    Add different difficulty levels (different ranges)

--- Program.cs
+++ Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
             Random random = new Random();
-            int randomNumber = random.Next(1,10);
-            int userGuess;
-            int limit = 10;
-            int numberOfAttempts = 0;
+            int gameLevel;
+            // int userGuess;
+            // int limit = 10;
+            // int numberOfAttempts = 0;
             bool exit = false;
-            //bool confirmation = true;
+            bool confirmation = true;
 
             // Implement different difficulty levels (different ranges): 
             // 
@@ -24,78 +24,48 @@
                 Console.WriteLine("2 for Intermediate - Random Numbers: (1,1000)");
                 Console.WriteLine("3 for Senior - Random Numbers: (1,10000)");
                 Console.WriteLine("4 for Exit the Program");
-                Console.Write("Choose the difficulty level to play: ");
+                Console.Write("Choose an option: ");
                 if (!int.TryParse(Console.ReadLine(), out int userLevelInput)) 
                 {
                     Console.WriteLine("Invalid input! Please enter a valid integer");
                     break;
                 }
 
+                var numberGuessing = new NumberGuess();
+
                 switch (userLevelInput)
                 {
                     case 1:
-                        // Junior
+                        Console.WriteLine("Junior Level");
+                        // do
+                        // {
+                        gameLevel = random.Next(1, 5);
+                        numberGuessing.NumberGuessingGame(gameLevel);
+
+                        // } while (confirmation);
                         break;
                     
                     case 2:
-                        // Intermediate
+                        Console.WriteLine("Intermediate Level");
+                        gameLevel = random.Next(1, 10);
+                        numberGuessing.NumberGuessingGame(gameLevel);
                         break;
 
                     case 3:
-                        // Senior
+                        Console.WriteLine("Senior Level");
+                        gameLevel = random.Next(1, 15);
+                        numberGuessing.NumberGuessingGame(gameLevel);
                         break;
                     case 4:
-                        // Goodbye!
+                        Console.WriteLine("Goodbye!");
+                        exit = true;
                         break;
                     
                     default:
+                        Console.WriteLine("Invalid option. Try again!");
                         break;
-                }
-
-            }
-
-
-
-            while (true) 
-            {
-                Console.Write("Try to guess the number: ");
-                while (true)
-                {
-                    Console.Write("Try to guess the number: ");
-                    if (int.TryParse(Console.ReadLine(), out userGuess))
-                    {
-                        break;
-                    }
-                    Console.WriteLine("Invalid input! Please enter a valid number."); 
-                }
-
-                if (userGuess == randomNumber)
-                {
-                    Console.WriteLine("Your guessed it. Congratulations!");
-                    int highscore = limit - numberOfAttempts;
-                    Console.WriteLine($"Your highscore was: {highscore}");
-                    break;
-                }
-
-                else if (userGuess != randomNumber)
-                {
-                    numberOfAttempts++;
-                    if (numberOfAttempts < limit)
-                    {
-                        string message = (userGuess > randomNumber) ? "Too high" : "Too low";
-                        Console.WriteLine(message);
-                        // score--;
-                        Console.WriteLine($"Number of guesses made: {numberOfAttempts}");
-                    }
-                    else 
-                    {
-                        Console.WriteLine("You runs out of attempts");
-                        break;
-                    }
-                    
                 }
             }
-
         }
     }
 }
